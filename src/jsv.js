b04952590d83eba82a957a1fdfaae17ede8c6c65
msynk <msynk@outlook.com>
2020-09-14 02:30:11 +0430

    add template and state functionality

--- src/jsv.js
+++ src/jsv.js
@@ -1,34 +1,88 @@
 var JSV = (function () {
-    var _containerId, _container, _context = {};
+    var _containerId, _container, _states = {};
 
     return {
         init: init,
+        template: template,
+        state: state
     };
 
     function init(id) {
         _containerId = id;
         _container = document.getElementById(id);
-        mount(_container);
-        return _context;
+        return mount(_container);
     }
 
     // ========================================================================
 
     function mount(element) {
-        mountElement(element);
-        if (element.children.length == 0) return;
-        for (var i = 0; i < element.children.length; i++) {
-            mountElement(element.children[i]);
+        var context = {};
+
+        m(element);
+
+        return context;
+
+        function m(el) {
+            mountElement(el);
+            if (el.children.length == 0) return;
+            for (var i = 0; i < el.children.length; i++) {
+                m(el.children[i]);
+            }
+        }
+
+        function mountElement(element) {
+            var attrs = element.attributes;
+            if (!attrs) return;
+            var idAttr = attrs['id'];
+            if (idAttr) {
+                if (context[idAttr.value])
+                    throw new Error('duplicate HTML element id: ' + idAttr.value);
+                context[idAttr.value] = element;
+            }
+
+            var templateAttr = attrs['template'];
+            if (templateAttr) {
+                var templateId = templateAttr.value;
+                var createTemplate = template(templateId);
+                element.appendChild(createTemplate());
+            }
         }
     }
 
-    function mountElement(element) {
-        var atts = element.attributes;
 
-        var idAttr = atts['id'];
-        if (idAttr) {
-            if (_context[idAttr.value]) throw new Error('duplicate HTML element id: ' + idAttrr.value);
-            _context[idAttr.value] = element;
+    // ========================================================================
+
+    function template(id) {
+        var template = document.getElementById(id);
+        if (!template || template.tagName.toLowerCase() !== 'template')
+            throw new Error('not a template: ' + id);
+        template.parentElement.removeChild(template);
+
+        return function t(binder) {
+            var node = template.content.cloneNode(true);
+            var context = mount(node);
+            binder && binder(context);
+            return node;
+
+            function getElementById(element, id) {
+                var idAttr = element.attributes['id'];
+                if (idAttr && idAttr.value === id) return element;
+
+                for (var i = 0; i < element.children.length; i++) {
+                    var found = getElementById(element.children[i], id);
+                    if (found) return found;
+                }
+            }
+        }
+    }
+
+    // ========================================================================
+
+    function state(name) {
+        var state = _states[name] || (_states[name] = []);
+        return {
+            pub: function (value) { state.forEach(function (fn) { fn(value); }); },
+            sub: function (fn) { state.push(fn); }
         }
     }
 })();
